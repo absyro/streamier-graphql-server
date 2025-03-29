2e8df88fc9887e1a460942d378cd1345c173710d
amirfarzamnia <amir2452fa@gmail.com>
2025-03-29 06:38:21 -0600

    improvements

--- Configuration/ServiceCollectionExtensions.cs
+++ Configuration/ServiceCollectionExtensions.cs
@@ -36,7 +36,7 @@ public static class ServiceCollectionExtensions
         });
 
         services.AddDbContext<Contexts.AppDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("Default"))
+            options.UseNpgsql(configuration.GetConnectionString("Postgres"))
         );
 
         services
