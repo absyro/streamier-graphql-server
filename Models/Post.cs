f971809dbf3067d851f2a0d01342fb7c29fb1116
amirfarzamnia <amir2452fa@gmail.com>
2025-04-17 08:06:09 -0600

    improvements

---
+++ Models/Post.cs
@@ -0,0 +1,56 @@
+namespace StreamierGraphQLServer.Models;
+
+using System.ComponentModel.DataAnnotations;
+using StreamierGraphQLServer.Models.Base;
+
+/// <summary>
+/// Represents a blog post in the system.
+/// </summary>
+public class Post : BaseEntity
+{
+    /// <summary>
+    /// The title of the blog post.
+    /// </summary>
+    /// <remarks>
+    /// Maximum length is 120 characters.
+    /// Minimum length is 5 characters.
+    /// </remarks>
+    [Required]
+    [StringLength(120, MinimumLength = 5)]
+    public required string Title { get; set; }
+
+    /// <summary>
+    /// The main content of the blog post in Markdown format.
+    /// </summary>
+    /// <remarks>
+    /// Maximum length is 50000 characters.
+    /// Minimum length is 500 characters.
+    /// </remarks>
+    [Required]
+    [StringLength(50000, MinimumLength = 500)]
+    public required string Content { get; set; }
+
+    /// <summary>
+    /// The author of this post.
+    /// </summary>
+    [Required]
+    public required User Author { get; set; }
+
+    /// <summary>
+    /// The collection of tags associated with this post.
+    /// </summary>
+    [Required]
+    public List<string> Tags { get; set; } = [];
+
+    /// <summary>
+    /// Whether the post is featured.
+    /// </summary>
+    [Required]
+    public bool IsFeatured { get; set; } = false;
+
+    /// <summary>
+    /// Collection of comments on this post.
+    /// </summary>
+    [Required]
+    public List<PostComment> Comments { get; set; } = [];
+}
---
+++ Models/PostComment.cs
@@ -0,0 +1,38 @@
+namespace StreamierGraphQLServer.Models;
+
+using System.ComponentModel.DataAnnotations;
+using StreamierGraphQLServer.Models.Base;
+
+/// <summary>
+/// Represents a comment on a blog post.
+/// </summary>
+public class PostComment : BaseEntity
+{
+    /// <summary>
+    /// The author of this comment.
+    /// </summary>
+    [Required]
+    public required User Author { get; set; }
+
+    /// <summary>
+    /// The content of the comment.
+    /// </summary>
+    /// <remarks>
+    /// This field has a maximum length of 1000 characters.
+    /// </remarks>
+    [Required]
+    [StringLength(1000)]
+    public required string Content { get; set; }
+
+    /// <summary>
+    /// The parent comment if this is a reply.
+    /// </summary>
+    [Required]
+    public PostComment? ParentComment { get; set; }
+
+    /// <summary>
+    /// Collection of replies to this comment.
+    /// </summary>
+    [Required]
+    public List<PostComment> Replies { get; set; } = [];
+}
